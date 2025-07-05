e670e6cae40d8ed7434d4e508de7d4090b8466ab
Nahuel <salvatierra.nahuel@gmail.com>
2025-07-05 02:10:59 -0300

    Add useLogout hook for user logout functionality, including error handling and loading state management

---
+++ src/app/hooks/useLogout.ts
@@ -0,0 +1,31 @@
+"use client";
+
+import { useUserStore } from "@/store/user-store";
+import { useAuth } from "./useAuth";
+import { toast } from "react-toastify";
+import { useState } from "react";
+
+export default function useLogout() {
+  const [loading, setLoading] = useState(false);
+  const { logout } = useAuth();
+
+  const setError = useUserStore((state) => state.setError);
+
+  const submit = async () => {
+    try {
+      setLoading(true);
+      await logout();
+      toast.success("Logged out successfully");
+    } catch (error) {
+      console.error(error);
+      toast.error("Logout failed");
+      const errorMessage =
+        error instanceof Error ? error.message : "Logout failed";
+      setError(errorMessage);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { submit, loading };
+}
