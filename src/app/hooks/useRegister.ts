0d79d10fbf5415b9fd1a9ec2a0a9d78a6e06aae4
Nahuel <salvatierra.nahuel@gmail.com>
2025-07-05 02:11:05 -0300

    Add useRegister hook for user registration functionality, integrating form handling with validation and loading states

---
+++ src/app/hooks/useRegister.ts
@@ -0,0 +1,46 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { loginSchema } from "@/app/validations/loginSchema";
+import { useAuth } from "./useAuth";
+import { useState } from "react";
+
+export default function useRegister({
+  onSuccess,
+  onError,
+}: {
+  onSuccess?: () => void;
+  onError?: () => void;
+}) {
+  const [loading, setLoading] = useState(false);
+  const { register } = useAuth();
+
+  const {
+    register: registerForm,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(loginSchema),
+  });
+
+  const submit = handleSubmit(async (data) => {
+    try {
+      setLoading(true);
+      await register(data);
+      onSuccess?.();
+    } catch (error: unknown) {
+      console.error(error);
+      onError?.();
+    } finally {
+      setLoading(false);
+    }
+  });
+
+  return {
+    validationErrors: errors,
+    submit,
+    registerForm,
+    loading,
+  };
+}
