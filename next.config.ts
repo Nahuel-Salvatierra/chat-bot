32789c8a5a23396adc5ed9960bb57cc6a9738066
Nahuel <salvatierra.nahuel@gmail.com>
2025-07-06 13:53:46 -0300

    Update Next.js configuration to include image remote patterns and environment variable for API base URL; add @radix-ui/react-aspect-ratio dependency

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
+  env: {
+    API_BASE_URL: process.env.API_BASE_URL,
+  },
 };
 
 export default nextConfig;
