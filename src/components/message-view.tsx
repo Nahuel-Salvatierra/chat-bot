30521e5dc5b62527bcee1eab87b1303354a49f18
Nahuel <salvatierra.nahuel@gmail.com>
2025-07-06 15:37:46 -0300

    Refactor MessageView component to utilize custom hook for message retrieval and add loading state with LoaderBox

--- src/components/message-view.tsx
+++ src/components/message-view.tsx
@@ -3,34 +3,16 @@
 import UserMessage from "./user-message";
 import AssistantMessage from "./assistant-message";
 import { useMessageStore } from "@/app/store/message-store";
-import { useEffect } from "react";
-import { Auth } from "@/domain/auth";
-import { env } from "@/configuration/environments";
+import { useGetMessages } from "@/app/hooks/use-get-messages";
+import LoaderBox from "./loader-box";
 
 export default function MessageView() {
-  const { addManyMessages } = useMessageStore((state) => state);
   const newMessage = useMessageStore((state) => state.messages);
-
-  useEffect(() => {
-    const fetchMessages = async () => {
-      const token = await Auth.getToken();
-      if (token) {
-        const messages = await fetch(`${env.baseUrl}/api/messages`, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
-        const data = await messages.json();
-        addManyMessages(data);
-      }
-    };
-    fetchMessages();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
+  const { loading } = useGetMessages();
   return (
     <div className="flex gap-2 w-full py-2 pb-4">
       <div className="flex flex-col gap-2 w-full">
+        {loading && <LoaderBox />}
         {newMessage.map((message) =>
           message.role === "user" ? (
             <UserMessage key={message.createdAt} message={message} />
--- src/components/register-form.tsx
+++ src/components/register-form.tsx
@@ -11,7 +11,7 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import useRegister from "@/app/hooks/useRegister";
+import useRegister from "@/app/hooks/use-register";
 import Link from "next/link";
 import { toast } from "react-toastify";
 import { useRouter } from "next/navigation";
