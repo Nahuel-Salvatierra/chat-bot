755f04000ac31a6dee313239cdc10c0e687bde5b
Nahuel <salvatierra.nahuel@gmail.com>
2025-07-06 13:54:19 -0300

    Add custom hook `useSendMessage` for handling message submission, including loading state, validation, and error handling.

---
+++ src/app/hooks/useSendMessage.ts
@@ -0,0 +1,69 @@
+"use client";
+
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { sendMessage } from "../actions/message.action";
+import { messageSchema } from "../validations/message-schema";
+import { Auth } from "@/domain/auth";
+import { createMessage, Message } from "@/domain/message";
+import { useMessageStore } from "@/app/store/message-store";
+import { MessageError } from "@/services/api/message.service";
+import { toast } from "react-toastify";
+
+export default function useSendMessage({
+  onSuccess,
+}: {
+  onSuccess?: (response: Message) => void;
+}) {
+  const [loading, setLoading] = useState(false);
+
+  const addMessage = useMessageStore((state) => state.addMessage);
+  const updateOne = useMessageStore((state) => state.updateOne);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({
+    resolver: yupResolver(messageSchema),
+  });
+
+  const submit = handleSubmit(async (data) => {
+    const newMessage = createMessage(
+      data.message,
+      "user",
+      new Date(),
+      "loading"
+    );
+    const token = await Auth.getToken();
+    setLoading(true);
+    addMessage(newMessage);
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      const response = await sendMessage(data.message, token);
+      updateOne({ ...newMessage, status: "success" });
+      onSuccess?.(response);
+      reset();
+      return response;
+    } catch (error) {
+      const messageError =
+        error instanceof MessageError
+          ? error
+          : new MessageError("Error desconocido");
+      toast.error(messageError.message);
+      updateOne({ ...newMessage, status: "error" });
+    } finally {
+      setLoading(false);
+    }
+  });
+
+  return {
+    loading,
+    registerForm: register,
+    submit,
+    validationErrors: errors,
+    messageStatus: status,
+  };
+}
