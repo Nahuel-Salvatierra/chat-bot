d52686411f3080767fa13df7db471ad552e9d28f
Nahuel <salvatierra.nahuel@gmail.com>
2025-07-06 15:36:54 -0300

    Add metadata for ProtectedLayout component to define page title and description

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from "next";
 import ProtectedGuard from "./guard";
+
+export const metadata: Metadata = {
+  title: "Chat with IA Assistant",
+  description: "Chat with IA Assistant",
+};
 
 export default function ProtectedLayout({
   children,
