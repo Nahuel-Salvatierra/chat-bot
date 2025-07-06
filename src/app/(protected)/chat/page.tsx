0ce486afb2f9a5ae73542da4097a5f58060c29e0
Nahuel <salvatierra.nahuel@gmail.com>
2025-07-06 13:53:55 -0300

    Add ChatPage component with UI for chat interface, including message view and form

---
+++ src/app/(protected)/chat/page.tsx
@@ -0,0 +1,19 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import MessageView from "../../../components/message-view";
+import MessageForm from "../../../components/message-form";
+
+export default async function ChatPage() {
+  return (
+    <div className="w-full h-screen flex p-4 ">
+      <Card className="w-full h-full flex flex-col justify-between">
+        <CardHeader>
+          <CardTitle>Chat with IA Assistant</CardTitle>
+        </CardHeader>
+        <CardContent className="overflow-y-auto max-w-2xl mx-auto">
+          <MessageView />
+          <MessageForm />
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
