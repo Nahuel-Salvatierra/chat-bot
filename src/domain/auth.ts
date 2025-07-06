f1c8a8a433da3f40e29916de8dbc43a241c23607
Nahuel <salvatierra.nahuel@gmail.com>
2025-07-06 13:55:16 -0300

    Add authentication module with login, registration, logout, and user retrieval methods using FirebaseAuth service

---
+++ src/domain/auth.ts
@@ -0,0 +1,43 @@
+import { FirebaseAuth } from "@/services/firebase/auth-firebase";
+import { IUser } from "./user";
+
+export type Login = {
+  email: string;
+  password: string;
+};
+
+export type Register = Login;
+
+export interface IAuth {
+  register: (data: Register) => Promise<IUser>;
+  login: (data: Login) => Promise<IUser>;
+  logout: () => Promise<void>;
+  getToken: () => Promise<string | undefined>;
+  getAuthUser: () => Promise<IUser | null>;
+}
+
+export const getAuth = () => {
+  return new FirebaseAuth();
+};
+
+export class Auth {
+  static async login(data: Login) {
+    return getAuth().login(data);
+  }
+
+  static async register(data: Register) {
+    return getAuth().register(data);
+  }
+
+  static async logout() {
+    return getAuth().logout();
+  }
+
+  static async getToken() {
+    return getAuth().getToken();
+  }
+
+  static async getAuthUser() {
+    return getAuth().getAuthUser();
+  }
+}
