a5b553cb4e579d0d7bc8ee0232ec1297832db1d9
Nahuel <salvatierra.nahuel@gmail.com>
2025-07-08 16:32:04 -0300

    Add loading indicator to UserMessage component with LoaderDots for enhanced user feedback

--- src/components/user-message.tsx
+++ src/components/user-message.tsx
@@ -5,16 +5,21 @@ import CheckButton from "./check-icon";
 import { Message } from "@/domain/message";
 import ErrorIcon from "./error-icon";
 import DoubleCheckButton from "./double-check-icon";
+import LoaderDots from "./loader-dots";
 
 export default function UserMessage({ message }: { message: Message }) {
   if (message.content == null) return null;
 
+  console.log(message.status);
   return (
-    <Label className="text-sm bg-secondary p-2 rounded-lg w-fit">
-      {message.content}
-      {message.status === "loading" && <CheckButton />}
-      {message.status === "error" && <ErrorIcon />}
-      {message.status === "success" && <DoubleCheckButton />}
-    </Label>
+    <>
+      <Label className="text-sm bg-secondary p-2 rounded-lg w-fit">
+        {message.content}
+        {message.status === "error" && <ErrorIcon />}
+        {message.status === "loading" && <CheckButton />}
+        {message.status === "success" && <DoubleCheckButton />}
+        {message.status === "loading" && <LoaderDots />}
+      </Label>
+    </>
   );
 }
